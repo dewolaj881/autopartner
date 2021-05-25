bbe43b38d14fd773c2585b3e260e19ece657b254
dewolaj881 <dewolaj881@gmail.com>
2021-05-25 23:27:49 +0200

    feat: showcase section

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Nav from '../components/Nav'
+import Showcase from '../components/Showcase'
 export default function Home() {
   return (
     <body className="bg-gray-900">
       <header>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet" />
-        <Nav></Nav>
+        <div>
+          <Nav></Nav>
+          <Showcase></Showcase>
+        </div>
       </header>
     </body>
   )
