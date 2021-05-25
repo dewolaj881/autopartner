c1b566504ce03cf8485e00d505f47047dca7e6f9
dewolaj881 <dewolaj881@gmail.com>
2021-05-25 15:05:08 +0200

    feat: russo one font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'russo': ['Russo One', 'sans-serif']
+      }
+    },
   },
   variants: {
     extend: {},
