51e1fc8fb0ff9e2e092f112e3b9e4c152a313313
dewolaj881 <dewolaj881@gmail.com>
2021-05-30 19:35:31 +0200

    feat: showacase & about components

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Nav from '../components/Nav'
 import Showcase from '../components/Showcase'
+import About from '../components/About'
 export default function Home() {
   return (
-    <body className="bg-gray-900">
+    <body className="bg-gray-900 ">
       <header>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet" />
         <div>
           <Nav></Nav>
           <Showcase></Showcase>
+          <About></About>
         </div>
       </header>
     </body>
