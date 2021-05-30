a5df76ebcd6abbdc9e50450b0d48dbde540bbdf5
dewolaj881 <dewolaj881@gmail.com>
2021-05-30 19:34:34 +0200

    feat: fully responsive showcase

--- components/Showcase.js
+++ components/Showcase.js
@@ -6,30 +6,17 @@ import React from "react";
 function Showcase() {
   return (
     <div>
-      {/* <div class="text-white font-bold  md:grid md:grid-cols-2 block text-center">
-        <h1 className="md:block hidden text-4xl">Szybka dostawa <br></br>jest naszym celem!</h1>
-        <h1 className="md:hidden block z-20 mt-12 shadow-xl text-center text-2xl">Szybka dostawa <br></br>jest naszym celem!</h1>
-        <img class="md:block hidden ml-52 relative top-0 right-16" src="../img/truck1.png" alt="" />
-        <img class="md:hidden block z-10 mx-auto" src="../img/truck1.png" alt="" />
-      </div>
-      <div class="md:grid md:grid-cols-2 block">
-        <img class="relative" src="../img/crossing1.png" alt="" />
-        <h1>test test test</h1>
-      </div> */}
-      <div class="text-white font-bold h-96 grid grid-cols-2">
-        <div className="mt-24 ml-52">
-          <h1 className="md:block text-4xl hidden">Szybka dostawa <br></br>jest naszym celem!</h1>
-          <p className="font-normal mt-4">Przy realizacji powierzonych nam zadań <br></br> cenimy sobie szybkość realizacji.</p>
-          <button class="bg-red-600 py-2 px-6 rounded-3xl mt-8 hover:bg-white hover:border-2 hover:border-red-600 hover:text-black">
+      <div class="text-white font-bold bg-cover bg-no-repeat  bg-center h-spacing-mobile md:h-screen -mt-10 md:-mt-36 items-center bg-hero-pattern">
+        <div className="block mx-auto md:mx-px">
+          <h1 className="block pt-36 md:pt-56 text-4xl  md:text-left text-center md:ml-24">Szybka dostawa <br></br>jest naszym celem!</h1>
+          <p className="block text-xl font-normal text-center md:text-left md:ml-24 mt-4 ">Przy realizacji powierzonych nam zadań <br></br> cenimy sobie szybkość realizacji </p>
+          <button class="md:block hidden bg-red-600  z-30 mb-36 ml-24 py-2 px-6 rounded-3xl mt-8 hover:bg-gray-900 hover:border-2 hover:border-white hover:text-white">
+            <p>Nawiąż współpracę</p>
+          </button>
+          <button class="md:hidden block mx-auto text-lg bg-red-600  z-30 mb-8 text-center py-1 px-2 rounded-3xl mt-4 hover:bg-gray-900 hover:border-2 hover:border-white hover:text-white">
             <p>Nawiąż współpracę</p>
           </button>
         </div>
-        <h1 className="md:hidden block relative z-10 text-center mt-24">Szybka dostawa <br></br>jest naszym celem!</h1>
-        <img class="md:block hidden absolute right-0 top-0 ml-48" src="../img/truck1.png" alt="" />
-        <img class="md:hidden block mx-0 absolute top-0 right-0.5" src="../img/truck1.png" alt="" />
-      </div>
-      <div class="grid grid-cols-2">
-        <img class="block  mt-56" src="../img/crossing1.png" alt="" />
       </div>
     </div>
   );
