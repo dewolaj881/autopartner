b739705901089b0595aded4ac7a937f2fc1890f9
dewolaj881 <dewolaj881@gmail.com>
2021-06-10 15:33:37 +0200

    feat: footer

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Footer() {
+
+  const [date, setDate] = useState();
+
+  const getYear = () => setDate(new Date().getFullYear())
+
+  useEffect(() => {
+    getYear();
+  }, [])
+  return (
+    <div className="bg-gray-900 border-t-2 text-sm border-red-600 py-4 text-center text-white mt-16">
+      &copy; Autopartner - {date}
+      <div>Designed by NaujaScript</div>
+    </div>
+  );
+}
